da111f7fb2d860a2f41277e3cd5e35573fb57a71
HADES-96 <108996607+HADES-96@users.noreply.github.com>
2022-10-24 18:37:10 +0200

    A translation for DE
    A translation for german people.

--- locales/en.lua
+++ locales/en.lua
@@ -1,11 +1,13 @@
-Locales['en'] = {
-  ['show_registration'] = 'show Registration Menu',
-  ['show_active_character'] = 'show Active Character',
-  ['delete_character'] = 'delete Your Character And Create A New One',
-  ['deleted_character'] = 'your character has been deleted.',
-  ['not_registered'] = 'you do not have a character reistered.',
-  ['active_character'] = '~b~active Character:~s~ %s %s',
-  ['already_registered'] = 'you already have a character registered.',
-  ['failed_identity'] = 'failed to set your character, try again later or contact the server admin!',
-  ['create_a_character'] = 'you have to create a character in order to play.'
+Locales['de'] = {
+  ['show_registration'] = 'Einreisemenü anzeigen',
+  ['show_active_character'] = 'alle Charakter anzeigen',
+  ['delete_character'] = 'Möchtest du deinen Charakter löschen und einen neuen erstellen?',
+  ['deleted_character'] = 'Dein Charakter wurde gelöscht.',
+  ['not_registered'] = 'Du hast keinen Charakter.',
+  ['active_character'] = '~b~aktueller Charakter :~s~ %s %s',
+  ['already_registered'] = 'Du hast bereits einen Charakter registriert.',
+  ['failed_identity'] = 'Du konntest deinen Charakter nicht festlegen, versuchen Sie es später noch einmal oder wende Dich an den Server-Administrator!',
+  ['create_a_character'] = 'Du musst einen Charakter erstellen, um spielen zu können.'
 }
+-- DO NOT FORGET to translate the HTML files as well ('placeholder' and so on)
+-- NICHT VERGESSEN die HTML Dateien ebenfalls zu übersetzen ('placeholder' und so weiter
